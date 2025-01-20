83e4fc387b8c21065f37ec895463b47f1e07752d
Yuval Mazor <yuvmaz@gmail.com>
2025-01-20 08:52:47 +0200

    Fix wrong kernel name

--- Lab 3 - Fine-Tune Bert for Sentiment Analysis/1_setup_conda.sh
+++ Lab 3 - Fine-Tune Bert for Sentiment Analysis/1_setup_conda.sh
@@ -1,4 +1,4 @@
 conda create -n bert_ft  --yes  python=3.10
 conda run    -n bert_ft  --live-stream conda install pip
 conda run    -n bert_ft  --live-stream pip install -r requirements.txt
-conda run    -n bert_ft  --live-stream python -m ipykernel install --user --name llama3_ft --display-name "Python (bert_ft)"
+conda run    -n bert_ft  --live-stream python -m ipykernel install --user --name bert_ft --display-name "Python (bert_ft)"
